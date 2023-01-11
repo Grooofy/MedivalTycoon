b5a986b1f9158f86b024c3a63c0bb214503e6cd9
userUnity <unitvladimir@gmail.com>
2023-01-11 22:25:18 +1000

    Visual + Controller

--- Assets/Scripts/Characters/CharacterInputController.cs
+++ Assets/Scripts/Characters/CharacterInputController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CharacterInputController : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick _joystick;
+    [SerializeField] private float _angelOffset = 70;
     
     private ICharacter _character;
+    private float _cosX;
+    private float _sinX;
     
     private void Awake()
     {
@@ -14,6 +17,12 @@ public class CharacterInputController : MonoBehaviour
         
         if (_character == null)
             throw new Exception($"There is no ICharacter component on the object: {gameObject.name}");
+    }
+
+    private void Start()
+    {
+        _cosX = Mathf.Cos(_angelOffset);
+        _sinX = Mathf.Sin(_angelOffset);
     }
 
     private void Update()
@@ -25,8 +34,21 @@ public class CharacterInputController : MonoBehaviour
     {
         float horizontal = _joystick.Horizontal;
         float vertical = _joystick.Vertical;
+        
+        float newHorizontal = CalculateOffSetX(horizontal, vertical);
+        float newVertical = CalculateOffSetY(horizontal, vertical);
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Vector3 direction = new Vector3(newHorizontal , 0f, newVertical);
         _character.Move(direction);
+    }
+
+    private float CalculateOffSetX(float x, float y)
+    {
+        return x * _cosX - y * _sinX;
+    }
+    
+    private float CalculateOffSetY(float x, float y)
+    {
+        return x * _sinX + y * _cosX;
     }
 }
